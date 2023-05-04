18cb019caf7559fe1dc0a1dd038f938de375860e
Emmanuel Oladipupo <emmanuel.oladipupo@decagon.dev>
2023-05-04 12:01:56 +0100

    project initialized

---
+++ src/main/java/com/logikcode/movieservicewithwebflux/controller/FluxAndMonoController.java
@@ -0,0 +1,4 @@
+package com.logikcode.movieservicewithwebflux.controller;
+
+public class FluxAndMonoController {
+}
---
+++ src/test/java/com/logikcode/movieservicewithwebflux/MovieServiceWithWebfluxApplicationTests.java
@@ -0,0 +1,13 @@
+package com.logikcode.movieservicewithwebflux;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MovieServiceWithWebfluxApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
